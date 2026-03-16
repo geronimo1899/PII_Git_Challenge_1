695dd5290f1bc6494f0eff83d8e078ef2a70f029
benavideslolo <lolobe06@gmail.com>
2026-03-16 18:39:33 -0300

    Cambio Incorrecto

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -12,7 +12,7 @@
 // Esta clase implementa la operación DIVISION
     public class Division
 {
-   public static double Divide(int a, int b)
+   public static double Dividir(int a, int b)
    {
        return 0; //Cambio Incorrecto
    }
